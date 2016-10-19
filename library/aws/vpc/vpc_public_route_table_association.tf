418b5b3bd5b1e5935bef6154f9247f69e2599c50
Timo Lehto <timo.lehto@appgyver.com>
2016-10-19 11:26:50 +0300

    refactor (aws/vpc) public subnet count var not needed

--- library/aws/vpc/vpc_public_route_table_association.tf
+++ library/aws/vpc/vpc_public_route_table_association.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "kontena_public" {
-  count          = "${var.aws_public_subnet_count}"
+  count          = "${length(var.aws_public_subnet_cidr_blocks)}"
 
   subnet_id      = "${element(aws_subnet.kontena_public.*.id, count.index)}"
   route_table_id = "${aws_route_table.kontena_public.id}"
--- library/aws/vpc/vpc_public_subnets.tf
+++ library/aws/vpc/vpc_public_subnets.tf
@@ -1,11 +1,9 @@
-variable "aws_public_subnet_count" { default = 3 }
-
 variable "aws_public_subnet_cidr_blocks" {
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 resource "aws_subnet" "kontena_public" {
-  count = "${var.aws_public_subnet_count}"
+  count = "${length(var.aws_public_subnet_cidr_blocks)}"
 
   vpc_id = "${aws_vpc.kontena.id}"
   cidr_block = "${var.aws_public_subnet_cidr_blocks[count.index]}"
