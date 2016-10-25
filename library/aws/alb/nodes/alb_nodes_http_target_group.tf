b9ff9d9e730774677cb4936445ff2666f06754e6
Matti Paksula <matti.paksula@iki.fi>
2016-10-25 14:46:58 +0300

    fix (aws/alb) Too long names..

--- library/aws/alb/nodes/alb_nodes_http_target_group.tf
+++ library/aws/alb/nodes/alb_nodes_http_target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_alb_target_group" "kontena_nodes_http" {
   # 32 char limit on name (!)
-  name     = "${var.name}-kontena-n-http"
+  name     = "${var.name}-k-n-http"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.kontena.id}"
--- library/aws/alb/nodes/https/alb_nodes_https_target_group.tf
+++ library/aws/alb/nodes/https/alb_nodes_https_target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_alb_target_group" "kontena_nodes_https" {
   # 32 char limit on name (!)
-  name     = "${var.name}-kontena-n-https"
+  name     = "${var.name}-k-n-https"
   port     = 443
   protocol = "HTTPS"
   vpc_id   = "${aws_vpc.kontena.id}"
